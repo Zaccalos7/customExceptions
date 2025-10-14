082cc8166bbe348bdaa161a33cff7bce8695e299
marco amleto guarino <guarin719@outlook.it>
2025-10-14 23:30:02 +0200

    added interface for internal test

---
+++ src/main/java/com/orbis/myUsage/MyUsageInterface.java
@@ -0,0 +1,7 @@
+package com.orbis.myUsage;
+
+import com.orbis.exception.annotations.ThrowRuntimeException;
+
+@ThrowRuntimeException(nameException = "MyUsageExceptions")
+public interface MyUsageInterface {
+}
