e577a1da88e31577bab8ada6c1a7a2bbb30734c6
marco amleto guarino <guarin719@outlook.it>
2025-10-18 12:45:21 +0200

    changed code style

--- src/main/java/com/orbis/exception/annotations/ExceptionMakerProcessor.java
+++ src/main/java/com/orbis/exception/annotations/ExceptionMakerProcessor.java
@@ -18,63 +18,63 @@ import java.util.Set;
 @SupportedAnnotationTypes("com.orbis.exception.annotations.ExceptionMaker")
 @SupportedSourceVersion(SourceVersion.RELEASE_23)
 public class ExceptionMakerProcessor extends AbstractProcessor {
-    @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        TypeElement packagePlusInterfaceName;
-        ExceptionMaker exceptionMaker;
-        String exceptionClassName;
-        String packageName;
-        Set<? extends Element> annotatedElementsWithThrowRuntimeException = roundEnv.getElementsAnnotatedWith(ExceptionMaker.class);
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+		TypeElement packagePlusInterfaceName;
+		ExceptionMaker exceptionMaker;
+		String exceptionClassName;
+		String packageName;
+		Set<? extends Element> annotatedElementsWithThrowRuntimeException = roundEnv.getElementsAnnotatedWith(ExceptionMaker.class);
 
-        for (Element element : annotatedElementsWithThrowRuntimeException) {
+		for (Element element : annotatedElementsWithThrowRuntimeException) {
 
-            if (element.getKind() != ElementKind.INTERFACE)
-                continue;
-            // Package where user created the interface with the annotation
-            packagePlusInterfaceName = (TypeElement) element;
-            exceptionMaker = packagePlusInterfaceName.getAnnotation(ExceptionMaker.class);
-            packageName = processingEnv.getElementUtils()
-                    .getPackageOf(packagePlusInterfaceName)
-                    .getQualifiedName()
-                    .toString();
+			if (element.getKind() != ElementKind.INTERFACE)
+				continue;
+			// Package where user created the interface with the annotation
+			packagePlusInterfaceName = (TypeElement) element;
+			exceptionMaker = packagePlusInterfaceName.getAnnotation(ExceptionMaker.class);
+			packageName = processingEnv.getElementUtils()
+					.getPackageOf(packagePlusInterfaceName)
+					.getQualifiedName()
+					.toString();
 
-            exceptionClassName = exceptionMaker.className();
-            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,
-                    "Exceptions generated from: " + packageName + "." + exceptionClassName);
+			exceptionClassName = exceptionMaker.className();
+			processingEnv.getMessager()
+					.printMessage(Diagnostic.Kind.NOTE, "Exceptions generated from: " + packageName + "." + exceptionClassName);
 
-            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,
-                    "Annotations taken: " + packagePlusInterfaceName + " the row= throwRuntimeException:" + exceptionMaker);
-            try {
-                JavaFileObject file = processingEnv.getFiler().createSourceFile(packageName + "." + exceptionClassName);
-               writeExceptionsFile(file, packageName, exceptionClassName);
-            } catch (Exception e) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "error:"+ e.getLocalizedMessage()+"/t"+ Arrays.toString(e.getStackTrace()));
-            }
-        }
-        return true;
-    }
+			processingEnv.getMessager()
+					.printMessage(Diagnostic.Kind.NOTE, "Annotations taken: " + packagePlusInterfaceName + " the row= throwRuntimeException:" + exceptionMaker);
+			try {
+				JavaFileObject file = processingEnv.getFiler().createSourceFile(packageName + "." + exceptionClassName);
+				writeExceptionsFile(file, packageName, exceptionClassName);
+			} catch (Exception e) {
+				processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "error:" + e.getLocalizedMessage() + "/t" + Arrays.toString(e.getStackTrace()));
+			}
+		}
+		return true;
+	}
 
-    public void writeExceptionsFile(JavaFileObject javaFileObject, String packageName, String exceptionClassName){
+	public void writeExceptionsFile(JavaFileObject javaFileObject, String packageName, String exceptionClassName) {
 
-        try (Writer writer = javaFileObject.openWriter()) {
-            writer.write("package " + packageName + ";\n\n");
-            writer.write("public class "+exceptionClassName + " extends RuntimeException {\n");
-            writer.write("    private final Object[] params;\n\n");
+		try (Writer writer = javaFileObject.openWriter()) {
+			writer.write("package " + packageName + ";\n\n");
+			writer.write("public class " + exceptionClassName + " extends RuntimeException {\n");
+			writer.write("    private final Object[] params;\n\n");
 
-            writer.write("    public " + exceptionClassName+" (String message) {\n");
-            writer.write("        super(message);\n");
-            writer.write("        this.params = null;\n");
-            writer.write("    }\n\n");
+			writer.write("    public " + exceptionClassName + " (String message) {\n");
+			writer.write("        super(message);\n");
+			writer.write("        this.params = null;\n");
+			writer.write("    }\n\n");
 
-            writer.write("    public "+exceptionClassName+"(String message, Object[] params) {\n");
-            writer.write("        super(message);\n");
-            writer.write("        this.params = params;\n");
-            writer.write("    }\n");
+			writer.write("    public " + exceptionClassName + "(String message, Object[] params) {\n");
+			writer.write("        super(message);\n");
+			writer.write("        this.params = params;\n");
+			writer.write("    }\n");
 
-            writer.write("}\n");
-        }catch (IOException exception){
-            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,"Error during the writting of the exception "+exceptionClassName);
-        }
+			writer.write("}\n");
+		} catch (IOException exception) {
+			processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Error during the writting of the exception " + exceptionClassName);
+		}
 
-    }
+	}
 }
--- src/main/java/com/orbis/exception/annotations/ExceptionRunnerProcessor.java
+++ src/main/java/com/orbis/exception/annotations/ExceptionRunnerProcessor.java
@@ -12,98 +12,106 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @AutoService(Processor.class)
 @SupportedAnnotationTypes("com.orbis.exception.annotations.ExceptionRunner")
 @SupportedSourceVersion(SourceVersion.RELEASE_23)
 public class ExceptionRunnerProcessor extends AbstractProcessor {
-    @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment) {
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment) {
 
-        Set<? extends Element> annotatedElementWithRunException = roundEnvironment.getElementsAnnotatedWith(ExceptionRunner.class);
-        ExecutableElement methodElement;
-        Element enclosingElement;
-        PackageElement packageElement;
-        List<? extends VariableElement> methodParameters;
-        List<String> typeParameters = new ArrayList<>();
+		Set<? extends Element> annotatedElementWithRunException =
+				roundEnvironment.getElementsAnnotatedWith(ExceptionRunner.class);
+		ExecutableElement methodElement;
+		Element enclosingElement;
+		PackageElement packageElement;
+		List<? extends VariableElement> methodParameters;
+		List<String> typeParameters = new ArrayList<>();
 
-        String methodName;
-        String interfaceName;
-        String packageName;
-        String exceptionNameClass;
-        String returnMethodType;
+		String methodName;
+		String interfaceName;
+		String packageName;
+		String exceptionNameClass;
+		String returnMethodType;
 
-        for(Element element : annotatedElementWithRunException){
-            if(element.getKind() != ElementKind.METHOD)
-                continue;
+		for (Element element : annotatedElementWithRunException) {
+			if (element.getKind() != ElementKind.METHOD) continue;
 
-            methodElement = (ExecutableElement) element;
-            methodName = methodElement.getSimpleName().toString();
-            returnMethodType = methodElement.getReturnType().toString();
-            methodParameters = methodElement.getParameters();
+			methodElement = (ExecutableElement) element;
+			methodName = methodElement.getSimpleName().toString();
+			returnMethodType = methodElement.getReturnType().toString();
+			methodParameters = methodElement.getParameters();
 
-            for(VariableElement param : methodParameters){
-                String paramName = param.getSimpleName().toString();
-                String paramType = param.asType().toString();
-                typeParameters.add(paramType+" "+paramName);
-            }
+			for (VariableElement param : methodParameters) {
+				String paramName = param.getSimpleName().toString();
+				String paramType = param.asType().toString();
+				typeParameters.add(paramType + " " + paramName);
+			}
 
-            exceptionNameClass = methodElement.getAnnotation(ExceptionRunner.class).exceptionMethod();
+			exceptionNameClass = methodElement.getAnnotation(ExceptionRunner.class).exceptionMethod();
 
-            enclosingElement = methodElement.getEnclosingElement();
-            interfaceName = enclosingElement.getSimpleName().toString();
+			enclosingElement = methodElement.getEnclosingElement();
+			interfaceName = enclosingElement.getSimpleName().toString();
 
-            packageElement = processingEnv.getElementUtils().getPackageOf(enclosingElement);
-            packageName = packageElement.getQualifiedName().toString();
+			packageElement = processingEnv.getElementUtils().getPackageOf(enclosingElement);
+			packageName = packageElement.getQualifiedName().toString();
 
-            processingEnv.getMessager().printMessage(
-                    Diagnostic.Kind.NOTE,
-                    "Annotated method: " + methodName
-            );
-            processingEnv.getMessager().printMessage(
-                    Diagnostic.Kind.NOTE,
-                    "Interface's name: " + interfaceName
-            );
-            processingEnv.getMessager().printMessage(
-                    Diagnostic.Kind.NOTE,
-                    "Package: " + packageName
-            );
+			processingEnv.getMessager()
+					.printMessage(Diagnostic.Kind.NOTE, "Annotated method: " + methodName);
+			processingEnv
+					.getMessager().printMessage(Diagnostic.Kind.NOTE, "Interface's name: " + interfaceName);
+			processingEnv.getMessager()
+					.printMessage(Diagnostic.Kind.NOTE, "Package: " + packageName);
 
-            processingEnv.getMessager().printMessage(
-                    Diagnostic.Kind.NOTE,
-                    "Exception class name: " + exceptionNameClass
-            );
+			processingEnv.getMessager()
+					.printMessage(Diagnostic.Kind.NOTE, "Exception class name: " + exceptionNameClass);
 
-            try {
-                JavaFileObject javaFileObject = processingEnv.getFiler().createSourceFile(packageName + "." + interfaceName+"Impl");
-                writeThrowCustomException(javaFileObject, methodName, packageName, interfaceName, exceptionNameClass, returnMethodType, typeParameters);
-            } catch (Exception e) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "error:"+ e.getLocalizedMessage()+"/t"+ Arrays.toString(e.getStackTrace()));
-            }
+			try {
+				JavaFileObject javaFileObject =
+						processingEnv.getFiler().createSourceFile(packageName + "." + interfaceName + "Impl");
+				writeThrowCustomException(
+						javaFileObject,
+						methodName,
+						packageName,
+						interfaceName,
+						exceptionNameClass,
+						returnMethodType,
+						typeParameters);
+			} catch (Exception e) {
+				processingEnv
+						.getMessager()
+						.printMessage(
+								Diagnostic.Kind.ERROR, "error:" + e.getLocalizedMessage() + "/t" + Arrays.toString(e.getStackTrace()));
+			}
+		}
+		return true;
+	}
 
-        }
-        return true;
-    }
+	public void writeThrowCustomException(
+			JavaFileObject javaFileObject,
+			String methodName,
+			String packageName,
+			String interfaceName,
+			String exceptionNameClass,
+			String returnMethodType,
+			List<String> typeParameters) {
+		String generatedNameClass = interfaceName + "Impl";
+		String methodArguments = String.join(",", typeParameters);
 
-    public void writeThrowCustomException(JavaFileObject javaFileObject, String methodName, String packageName, String interfaceName, String exceptionNameClass, String returnMethodType, List<String> typeParameters){
-        String generatedNameClass = interfaceName + "Impl";
-        String methodArguments = String.join(",", typeParameters);
+		try (Writer writer = javaFileObject.openWriter()) {
+			writer.write("package " + packageName + ";\n\n");
+			writer.write("import " + packageName + "." + exceptionNameClass + ";\n\n");
 
-        try (Writer writer = javaFileObject.openWriter()) {
-            writer.write("package " + packageName + ";\n\n");
-            writer.write("import " + packageName + "."+exceptionNameClass+";\n\n");
+			writer.write("public class " + generatedNameClass + " implements " + interfaceName + " {\n\n");
 
-            writer.write("public class " + generatedNameClass+" implements "+interfaceName + " {\n\n");
+			writer.write("\t@Override\n");
+			writer.write("    public " + returnMethodType + " " + methodName + "(" + methodArguments + ")" + " {\n");
+			writer.write("        throw new CustomException(message);\n");
+			writer.write("    }\n\n");
 
-            writer.write("\t@Override\n");
-            writer.write("    public "+returnMethodType+" "+methodName+"("+methodArguments+")" +" {\n");
-            writer.write("        throw new CustomException(message);\n");
-            writer.write("    }\n\n");
-
-            writer.write("}\n");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+			writer.write("}\n");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
