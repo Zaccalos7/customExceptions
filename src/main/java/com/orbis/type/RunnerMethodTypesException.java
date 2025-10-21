e8cc6764d85351a95fa33207d0a57ae522cb49cf
marco amleto guarino <guarin719@outlook.it>
2025-10-21 20:34:11 +0200

    add class for handling caos

---
+++ src/main/java/com/orbis/type/RunnerMethodTypesException.java
@@ -0,0 +1,76 @@
+package com.orbis.type;
+
+public class RunnerMethodTypesException {
+    private String componentModel;
+    private String generatedNameClass;
+    private String interfaceName;
+    private String returnMethodType;
+    private String methodName;
+    private String methodArguments;
+    private String exceptionNameClass;
+    private String variableList;
+
+    public String getComponentModel() {
+        return componentModel;
+    }
+
+    public void setComponentModel(String componentModel) {
+        this.componentModel = componentModel;
+    }
+
+    public String getGeneratedNameClass() {
+        return generatedNameClass;
+    }
+
+    public void setGeneratedNameClass(String generatedNameClass) {
+        this.generatedNameClass = generatedNameClass;
+    }
+
+    public String getInterfaceName() {
+        return interfaceName;
+    }
+
+    public void setInterfaceName(String interfaceName) {
+        this.interfaceName = interfaceName;
+    }
+
+    public String getReturnMethodType() {
+        return returnMethodType;
+    }
+
+    public void setReturnMethodType(String returnMethodType) {
+        this.returnMethodType = returnMethodType;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public String getMethodArguments() {
+        return methodArguments;
+    }
+
+    public void setMethodArguments(String methodArguments) {
+        this.methodArguments = methodArguments;
+    }
+
+    public String getExceptionNameClass() {
+        return exceptionNameClass;
+    }
+
+    public void setExceptionNameClass(String exceptionNameClass) {
+        this.exceptionNameClass = exceptionNameClass;
+    }
+
+    public String getVariableList() {
+        return variableList;
+    }
+
+    public void setVariableList(String variableList) {
+        this.variableList = variableList;
+    }
+}
