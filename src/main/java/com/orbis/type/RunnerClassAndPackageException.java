9f5763da15bb7ee21be54c344fc5e20034533701
marco amleto guarino <guarin719@outlook.it>
2025-10-23 22:43:15 +0200

    for javadoc

--- src/main/java/com/orbis/type/RunnerClassAndPackageException.java
+++ src/main/java/com/orbis/type/RunnerClassAndPackageException.java
@@ -1,26 +1,40 @@
 package com.orbis.type;
 
+/**
+ * Simple class holding package and interface names
+ * used during exception class generation.
+ */
 public class RunnerClassAndPackageException {
-    String packageName;
-    String interfaceName;
+    private String packageName;
+    private String interfaceName;
 
+    /**
+     * Creates a new container with package and interface name.
+     *
+     * @param packageName   the package name
+     * @param interfaceName the interface name
+     */
     public RunnerClassAndPackageException(String packageName, String interfaceName) {
         this.packageName = packageName;
         this.interfaceName = interfaceName;
     }
 
+    /** @return the package name */
     public String getPackageName() {
         return packageName;
     }
 
+    /** @param packageName the package name to set */
     public void setPackageName(String packageName) {
         this.packageName = packageName;
     }
 
+    /** @return the interface name */
     public String getInterfaceName() {
         return interfaceName;
     }
 
+    /** @param interfaceName the interface name to set */
     public void setInterfaceName(String interfaceName) {
         this.interfaceName = interfaceName;
     }
--- src/main/java/com/orbis/type/RunnerMethodTypesException.java
+++ src/main/java/com/orbis/type/RunnerMethodTypesException.java
@@ -1,5 +1,13 @@
 package com.orbis.type;
 
+import com.orbis.exception.annotations.ExceptionRunner;
+
+/**
+ * Class representing metadata of a method annotated with {@link ExceptionRunner}.
+ *
+ * <p>It stores return type, method name, arguments, the related exception class,
+ * and the variable list used for code generation.</p>
+ */
 public class RunnerMethodTypesException {
     private String returnMethodType;
     private String methodName;
@@ -7,42 +15,52 @@ public class RunnerMethodTypesException {
     private String exceptionNameClass;
     private String variableList;
 
+    /** @return the return type of the method */
     public String getReturnMethodType() {
         return returnMethodType;
     }
 
+    /** @param returnMethodType the return type to set */
     public void setReturnMethodType(String returnMethodType) {
         this.returnMethodType = returnMethodType;
     }
 
+    /** @return the method name */
     public String getMethodName() {
         return methodName;
     }
 
+    /** @param methodName the method name to set */
     public void setMethodName(String methodName) {
         this.methodName = methodName;
     }
 
+    /** @return the method arguments as a string */
     public String getMethodArguments() {
         return methodArguments;
     }
 
+    /** @param methodArguments the method arguments to set */
     public void setMethodArguments(String methodArguments) {
         this.methodArguments = methodArguments;
     }
 
+    /** @return the exception class name */
     public String getExceptionNameClass() {
         return exceptionNameClass;
     }
 
+    /** @param exceptionNameClass the exception class name to set */
     public void setExceptionNameClass(String exceptionNameClass) {
         this.exceptionNameClass = exceptionNameClass;
     }
 
+    /** @return the variable list (parameter names) */
     public String getVariableList() {
         return variableList;
     }
 
+    /** @param variableList the variable list to set */
     public void setVariableList(String variableList) {
         this.variableList = variableList;
     }
