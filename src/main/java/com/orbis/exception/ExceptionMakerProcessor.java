9b065cd2715a4a9951e89855b842b48e14e2d5dc
marco amleto guarino <guarin719@outlook.it>
2025-10-26 02:16:10 +0200

    Created a method to define the order and number of parameters

--- src/main/java/com/orbis/exception/ExceptionMakerProcessor.java
+++ src/main/java/com/orbis/exception/ExceptionMakerProcessor.java
@@ -34,6 +34,7 @@ public class ExceptionMakerProcessor extends AbstractProcessor {
         TypeElement packagePlusInterfaceName;
         ExceptionMaker exceptionMaker;
         String[] exceptionClassesName;
+        boolean isEnableCustomParameterOrder;
         String packageName;
         Set<? extends Element> annotatedElementsWithThrowRuntimeException = roundEnv.getElementsAnnotatedWith(ExceptionMaker.class);
 
@@ -50,33 +51,105 @@ public class ExceptionMakerProcessor extends AbstractProcessor {
                     .toString();
 
             exceptionClassesName = exceptionMaker.classesName();
+            isEnableCustomParameterOrder = exceptionMaker.enableCustomParameterOrder();
             processingEnv.getMessager()
                     .printMessage(Diagnostic.Kind.NOTE, "Making Exceptions from: " + packageName + "." + exceptionClassesName);
 
             processingEnv.getMessager()
                     .printMessage(Diagnostic.Kind.NOTE, "Annotations taken: " + packagePlusInterfaceName + " the row= ExceptionMaker:" + exceptionMaker);
-            try {
-                JavaFileObject javaFileObject;
-                for (String exceptionClassName : exceptionClassesName) {
-                    javaFileObject = processingEnv.getFiler().createSourceFile(packageName + "." + exceptionClassName);
-                    writeExceptionsFile(javaFileObject, packageName, exceptionClassName);
-                }
 
-            } catch (Exception e) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "error:" + e.getLocalizedMessage() + "/t" + Arrays.toString(e.getStackTrace()));
+            if (isEnableCustomParameterOrder) {
+                writeWithCustomParameterOrder(exceptionClassesName, packageName);
+            } else {
+                writeWithoutCustomParameterOrder(exceptionClassesName, packageName);
             }
         }
         return true;
     }
 
     /**
-     * Write the exceptions class
+     * Generates Java source files for the specified exception classes with applying custom parameter ordering.
+     * The number of parameters depends on those defined in the constructor.
      *
-     * @param javaFileObject java file to generate
-     * @param packageName the package where user used the annotations
+     * @param exceptionClassesName array of exception class names to generate
+     * @param packageName          name of the package where the classes will be created
+     */
+    private void writeWithCustomParameterOrder(String[] exceptionClassesName, String packageName) {
+        try {
+            JavaFileObject javaFileObject;
+
+            for (String exceptionClassName : exceptionClassesName) {
+                javaFileObject = processingEnv.getFiler().createSourceFile(packageName + "." + exceptionClassName);
+                writeExceptionsFileWithCustomParameterOrder(javaFileObject, packageName, exceptionClassName);
+            }
+
+        } catch (Exception e) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "error:" + e.getLocalizedMessage() + "/t" + Arrays.toString(e.getStackTrace()));
+        }
+    }
+
+    /**
+     * Writes a Java source file for a custom exception class, applying a specific parameter order.
+     * The generated class includes a varargs constructor and stores parameters in a single field.
+     *
+     * @param javaFileObject     the file object representing the source file to be written
+     * @param packageName        the package where the exception class will be placed
+     * @param exceptionClassName the name of the exception class to generate
+     */
+    private void writeExceptionsFileWithCustomParameterOrder(JavaFileObject javaFileObject, String packageName, String exceptionClassName) {
+        try (Writer writer = javaFileObject.openWriter()) {
+            writer.write("/*\n");
+            writer.write("*\n");
+            writer.write("* Code generated:\n");
+            writer.write("*" + " " + LocalDateTime.now() + "\n");
+            writer.write("*/\n");
+            writer.write("package " + packageName + ";\n\n");
+            writer.write("public class " + exceptionClassName + " extends RuntimeException {\n");
+
+            writer.write("    private final Object param;\n\n");
+
+            writer.write("    public " + exceptionClassName + "(Object... param) {\n");
+            writer.write("        this.param = param;\n");
+            writer.write("    }\n");
+
+            writer.write("  public Object getParam(){\n");
+            writer.write("      return param;\n");
+            writer.write("  }\n");
+
+            writer.write("}\n");
+        } catch (IOException exception) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Error during the writting of the exception " + exceptionClassName);
+        }
+    }
+
+    /**
+     * Generates Java source files for the specified exception classes without applying custom parameter ordering.
+     *
+     * @param exceptionClassesName array of exception class names to generate
+     * @param packageName          name of the package where the classes will be created
+     */
+    private void writeWithoutCustomParameterOrder(String[] exceptionClassesName, String packageName) {
+        try {
+            JavaFileObject javaFileObject;
+
+            for (String exceptionClassName : exceptionClassesName) {
+                javaFileObject = processingEnv.getFiler().createSourceFile(packageName + "." + exceptionClassName);
+                writeExceptionsFileWithoutCustomParameterOrder(javaFileObject, packageName, exceptionClassName);
+            }
+
+        } catch (Exception e) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "error:" + e.getLocalizedMessage() + "/t" + Arrays.toString(e.getStackTrace()));
+        }
+    }
+
+    /**
+     * Write the exceptions class, without custom parameter order
+     *
+     * @param javaFileObject     java file to generate
+     * @param packageName        the package where user used the annotations
      * @param exceptionClassName exception custom name
      */
-    public void writeExceptionsFile(JavaFileObject javaFileObject, String packageName, String exceptionClassName) {
+    public void writeExceptionsFileWithoutCustomParameterOrder(JavaFileObject javaFileObject, String packageName, String exceptionClassName) {
 
         try (Writer writer = javaFileObject.openWriter()) {
             writer.write("/*\n");
@@ -97,6 +170,10 @@ public class ExceptionMakerProcessor extends AbstractProcessor {
             writer.write("        super(message);\n");
             writer.write("        this.params = params;\n");
             writer.write("    }\n");
+
+            writer.write("  public Object[] getParams(){\n");
+            writer.write("      return params;\n");
+            writer.write("  }\n");
 
             writer.write("}\n");
         } catch (IOException exception) {
