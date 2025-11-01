29ab0c49dd80ac847d3943b62008f45699613c67
marco amleto guarino <guarin719@outlook.it>
2025-11-01 13:34:02 +0100

    added enum for store project store data

---
+++ src/main/java/com/orbis/exception/info/PROJECT.java
@@ -0,0 +1,20 @@
+package com.orbis.exception.info;
+
+/**
+ * Class for store current project verion and
+ * annotations package
+ */
+public enum PROJECT {
+    VERSION("1.0.3"),
+    PACKAGE("com.orbis.exception.annotations");
+
+    private final String value;
+
+    PROJECT(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
